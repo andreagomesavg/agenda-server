8740bebbdfcfbc888a9fcb27437e941a4cf3f40a
Andrea Gomes <andreagomes.avg@gmail.com>
2025-01-24 14:18:40 +0100

    ajustes auth

--- src/Auth/Auth.module.ts
+++ src/Auth/Auth.module.ts
@@ -3,6 +3,7 @@ import { AuthController } from './auth.controller';
 import { UsuariosService } from 'src/usuarios/usuarios.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema, Usuario } from 'src/usuarios/entities/usuario.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: 
@@ -11,7 +12,12 @@ import { UserSchema, Usuario } from 'src/usuarios/entities/usuario.entity';
       [
         {name: Usuario.name, schema: UserSchema}
       ]
-    )],
+    ),
+  JwtModule.register({
+    global: true,
+    secret: process.env.JWT_SECRET,
+    signOptions: { expiresIn: "60s"}
+  })],
   controllers: [AuthController],
   providers: [UsuariosService]
 })
--- src/Auth/auth.controller.ts
+++ src/Auth/auth.controller.ts
@@ -1,16 +1,37 @@
 import { UsuariosService } from './../usuarios/usuarios.service';
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpException, HttpStatus, Inject, Post, Req } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcryptjs from "bcryptjs";
+import { Request } from 'express';
 import { CreateUsuarioDto } from 'src/usuarios/dto/create-usuario.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor (@Inject() private usuariosService:UsuariosService){}
+    constructor (@Inject() private usuariosService:UsuariosService, @Inject() private jwtService:JwtService){}
 
     @Post("/register")
     async registarNuevoUsuario(@Body() createUsuarioDto:CreateUsuarioDto){
         createUsuarioDto.password = await bcryptjs.hash(createUsuarioDto.password, 10);
         return this.usuariosService.create(createUsuarioDto);
+    }
 
+    @HttpCode(HttpStatus.OK)
+    @Post("/login")
+    async iniciarSesion(@Body() usuario:CreateUsuarioDto){
+        const elUsuario = await this.usuariosService.findByUserName(usuario.username);
+        if(elUsuario==null) throw new HttpException("Acceso denegado", HttpStatus.NOT_FOUND)
+            const passOk = await bcryptjs.compare( usuario.password, elUsuario.password);
+            if (!passOk) throw new HttpException("Contrasena no valida", HttpStatus.NOT_FOUND);
+
+            const payload = {username: elUsuario.username, rol:elUsuario.rol}
+            const miToken = await this.jwtService.signAsync(payload);
+
+
+        return {access_token: miToken}
+    }
+
+    @Get("/validar")
+    async ValidarToken(@Req() req:Request){
+        
     }
 }
